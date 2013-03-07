19d8e614124bef1718b3d83e00176d4caf331f2a
zeekay <zeekayy@gmail.com>
2013-03-07 13:08:21 -0800

    Get absolute path to README, fixes #2.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
+from os import path
+
+readme = open(path.join(path.abspath(path.dirname(__file__)), 'README.md')).read()
 
 setup(
     name='bottle-websocket',
@@ -7,6 +10,6 @@ setup(
     author_email='zeekayy@gmail.com',
     packages=['bottle_websocket',],
     description='WebSockets for bottle',
-    long_description=open('README.md').read(),
+    long_description=readme,
     install_requires=['bottle', 'gevent-websocket'],
 )
